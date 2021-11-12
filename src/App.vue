6020f37aef834a15cb97236919293565ff349a86
John Bradley <johnbradley2008@gmail.com>
2021-11-12 08:37:27 -0500

    Improve app header

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,21 @@
   <v-app>
     <v-app-bar app>
       <v-icon class="ma-2">mdi-graph</v-icon>
-      <h3>Variant Art</h3>
+      <h3 class="text-no-wrap">Variant Art</h3>
       <v-spacer></v-spacer>
       <router-link to="/">
-        <v-btn small text> Concept </v-btn>
+        <v-btn text> Concept </v-btn>
       </router-link>
       <router-link to="/Create">
-        <v-btn small text> Create </v-btn>
+        <v-btn text> Create </v-btn>
       </router-link>
       <v-btn
-        small
+        fab
+        text
         href="https://github.com/johnbradley/variant-art"
         target="_blank"
-        text
       >
-        Code
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
